4ca0da136179ac7d1d94429fcae3ce6fb6c7e5b8
Daiki Hirose <speeed131@gmail.com>
2021-08-30 23:06:07 +0900

    apiのフロント取得とimage表示まで

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { api } from './api/index';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/" exact children={<Index />} />
+//         <Route path="/about" children={<About />} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+
 
 function App() {
+
+  const [images, setImages] = useState([] as any[]);
+
+  useEffect((): any => {
+    async function fetchImages() {
+      const res = await api.getImagesAtRandom()
+      setImages(res)
+      return res;
+    }
+
+      fetchImages()
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <ul>
+        {images.map((image, index) => (
+           <li key={index}>{image.page_url}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,3 +53,5 @@ function App() {
 }
 
 export default App;
+
+
