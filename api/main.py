2c0b1d15871e6cba202878993d2638b6824de6d2
Daiki Hirose <speeed131@gmail.com>
2021-08-30 15:56:01 +0900

    frontend追加

--- api/main.py
+++ api/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from api.routers import auth, fav, images
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 app.include_router(auth.router)
 app.include_router(fav.router)
 app.include_router(images.router)
