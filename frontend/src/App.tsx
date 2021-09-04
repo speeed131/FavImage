c0bfdc332d07c6b992dd8a3058fca0a2150b7f53
Daiki Hirose <speeed131@gmail.com>
2021-09-04 14:37:08 +0900

    ユーザー登録機能作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
 import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 // import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { api } from './api/index';
 import { IImage } from './interfaces/api';
+
+
+import SignIn from "./pages/SignIn";
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/sign-up" component={SignUp} exact />
+        <Route path="/sign-in" component={SignIn} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
 
 // function App() {
 //   return (
@@ -16,44 +36,53 @@ import { IImage } from './interfaces/api';
 //   );
 // }
 
+// function App() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
 
-function App() {
+// ReactDOM.render(<App />, document.querySelector('#app'));
 
-  const [images, setImages] = useState<IImage[] | []>([]);
+// function App() {
 
-  useEffect((): void => {
-    async function fetchImages() {
-      const res = await api.image.getImagesAtRandom()
-      res === undefined ? setImages([]) : setImages(res)
-    }
-    fetchImages()
-    }, []);
+//   const [images, setImages] = useState<IImage[] | []>([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-        {images.map((image, index) => (
-           <li key={index}>{image.page_url}</li>
-          ))}
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button>
-          fawfwaf
-        </button>
-      </header>
-    </div>
-  );
-}
+//   useEffect((): void => {
+//     async function fetchImages() {
+//       const res = await api.image.getImagesAtRandom()
+//       res === undefined ? setImages([]) : setImages(res)
+//     }
+//     fetchImages()
+//     }, []);
 
-export default App;
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <ul>
+//         {images.map((image, index) => (
+//            <li key={index}>{image.page_url}</li>
+//           ))}
+//         </ul>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <button>
+//           fawfwaf
+//         </button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
