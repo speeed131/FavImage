47e502597bc328a55538fa1e6c13d11587612fa6
Daiki Hirose <speeed131@gmail.com>
2021-10-18 16:22:59 +0900

    lint

--- api/routers/fav.py
+++ api/routers/fav.py
@@ -9,21 +9,21 @@ import api.cruds.auth as auth_cruds
 import api.schemas.auth as auth_schemas
 
 
-
 from api.db import get_db
 
 
-
 router = APIRouter()
+
 
 @router.post("/fav/images", response_model=fav_schemas.FavoriteImageResponse)
 async def post_fav_image(
     image_body: fav_schemas.FavoriteImage,
     db: AsyncSession = Depends(get_db),
     current_user: auth_schemas.User = Depends(auth_cruds.get_current_user)
-    ):
-#  [バックエンド] 画像を1枚お気に入り登録できる
+):
+    #  [バックエンド] 画像を1枚お気に入り登録できる
     return await fav_cruds.create_favorite_image(db, image_body)
+
 
 @router.get("/fav/images", response_model=List[fav_schemas.FavoriteImage])
 async def get_fav_images(
@@ -34,10 +34,12 @@ async def get_fav_images(
     user_id: int = current_user.id
     return await fav_cruds.read_favorite_images_by_user(db, user_id)
 
+
 @router.delete("/fav/images", response_model=fav_schemas.DeleteFavoriteImageResponse)
 def delete_fav_images():
     # [バックエンド] お気に入り登録した画像を削除できる
     pass
+
 
 @router.get("/fav/images/{category_name}", response_model=List[images_schemas.Image])
 def get_fav_images_by_category_name():
